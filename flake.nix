3224d2afa57ceebca40d11c6228e9c08e15f1501
bakaq <bakaq@bakaq.net>
2024-10-02 23:43:26 -0300

    flake.nix

---
+++ flake.nix
@@ -0,0 +1,37 @@
+{
+  description = "A helper to calculate ";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { nixpkgs, flake-utils, rust-overlay, ... }:
+    let 
+      overlays = [ 
+        (import rust-overlay)
+        (self: super: {
+          rustToolchain = super.rust-bin.stable.latest.default.override {
+            extensions = [ "rust-src" "rust-analyzer" ];
+          };
+        })
+      ];
+    in flake-utils.lib.eachDefaultSystem(system:
+      let 
+        pkgs = import nixpkgs { inherit system overlays; };
+      in
+      {
+        devShells = {
+          default = pkgs.mkShell {
+            buildInputs = with pkgs; [
+              rustToolchain
+            ];
+          };
+        };
+      }
+    );
+}
