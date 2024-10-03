8bea9679b821a124c9afaf7d1716728e1e00d0eb
bakaq <bakaq@bakaq.net>
2024-10-03 05:25:47 -0300

    Fix test

--- crates/market-helper-core/src/tests.rs
+++ crates/market-helper-core/src/tests.rs
@@ -1,18 +1,21 @@
+use crate::ItemDescription;
+
 use super::{ItemData, NutritionalTable};
 use approx::assert_relative_eq;
 
 #[test]
 fn item_data_basic() {
-    let item_data = ItemData::new(
-        "A",
-        1.0,
-        1.0,
-        NutritionalTable {
+    let item_data: ItemData = ItemDescription {
+        name: "A".into(),
+        price: 1.0,
+        weight: 1.0,
+        nutrition: NutritionalTable {
             portion_weight: 1.0,
             calories: 42.0,
             protein: 39.0,
         },
-    );
+    }
+    .into();
     let prices = item_data.prices();
 
     assert_relative_eq!(prices.calories, 42.0);
