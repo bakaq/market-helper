6777148f62c999dd4b75b46b039b2a4a0d4bbd05
bakaq <bakaq@bakaq.net>
2024-10-05 03:36:48 -0300

    Basic item list

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,6 +14,15 @@ function App() {
 function ItemList() {
   const [items, setItems] = useState([]);
 
+  async function getItems() {
+    const response = await fetch(`${API_ROOT}/item/get_all`, {
+      method: "GET",
+      headers: { "Accept": "application/json" },
+    });
+    const content = await response.json();
+    setItems(content);
+  }
+
   useEffect(() => {
     async function getItems() {
       const response = await fetch(`${API_ROOT}/item/get_all`, {
@@ -30,14 +39,15 @@ function ItemList() {
 
   const itemList = items.map((item) =>
     <li key={item.id}>
-      <ItemCard itemData={item.item}/>
+      <ItemCard itemData={item.item} />
     </li>
   );
 
   return (
     <div className="item-list">
-    <h1>Items</h1>
-    <ul className="item-list">{itemList}</ul>
+      <h2>Items</h2>
+      <button onClick={getItems}>Reload</button>
+      <ul className="item-list">{itemList}</ul>
     </div>
   );
 }
@@ -45,9 +55,30 @@ function ItemList() {
 function ItemCard({ itemData }) {
   return (
     <div className="item-card">
-      {itemData.name}
+      <ul>
+        <li>Name: {itemData.name}</li>
+        <li>Price: R$ {itemData.price}</li>
+        <li>Weight: {itemData.weight}g</li>
+        <li>
+          Nutritional table: <NutritionalTable nutrition={itemData.nutrition} />
+        </li>
+      </ul>
+
     </div>
   );
+}
+
+function NutritionalTable({ nutrition }) {
+  console.log(nutrition);
+  return (
+    <div>
+      <ul>
+        <li>Portion weight: {nutrition.portion_weight}g</li>
+        <li>Calories: {nutrition.calories}g</li>
+        <li>Protein: {nutrition.protein}g</li>
+      </ul>
+    </div>
+  )
 }
 
 export default App;
