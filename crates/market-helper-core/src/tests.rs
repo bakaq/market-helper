597afded0e36a603286c359aa350e37ed578f7d0
bakaq <bakaq@bakaq.net>
2024-10-07 21:02:08 -0300

    Fix tests

--- crates/market-helper-core/src/tests.rs
+++ crates/market-helper-core/src/tests.rs
@@ -12,12 +12,20 @@ fn item_data_basic() {
         nutrition: NutritionalTable {
             portion_weight: 1.0,
             calories: 42.0,
+            carbohidrates: 23.0,
             protein: 39.0,
+            total_fat: 123.0,
+            saturated_fat: 22.0,
+            fiber: 34.0,
         },
     }
     .into();
     let prices = item_data.prices();
 
     assert_relative_eq!(prices.calories, 42.0);
+    assert_relative_eq!(prices.carbohidrates, 23.0);
     assert_relative_eq!(prices.protein, 39.0);
+    assert_relative_eq!(prices.total_fat, 123.0);
+    assert_relative_eq!(prices.saturated_fat, 22.0);
+    assert_relative_eq!(prices.fiber, 34.0);
 }
